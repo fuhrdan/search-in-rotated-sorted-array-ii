25ec5b4036a2ebb8cc792e5fbe7dfe59a7b5417b
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2026-03-02 06:55:40 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿//*****************************************************************************
+//** 81. Search in Rotated Sorted Array II                          leetcode **
+//*****************************************************************************
+//** A rotated path with shadows cast by twins,
+//** Where ordered halves and ambiguity begin.
+//** We split by mid and test which side is true,
+//** And trim the edge till target comes to view.
+//*****************************************************************************
+
+bool search(int* nums, int numsSize, int target) {
+    int l = 0;
+    int r = numsSize - 1;
+    int mid;
+
+    while (l < r)
+    {
+        mid = (l + r) >> 1;
+
+        if (nums[mid] > nums[r])
+        {
+            if (nums[l] <= target && target <= nums[mid])
+            {
+                r = mid;
+            }
+            else
+            {
+                l = mid + 1;
+            }
+        }
+        else if (nums[mid] < nums[r])
+        {
+            if (nums[mid] < target && target <= nums[r])
+            {
+                l = mid + 1;
+            }
+            else
+            {
+                r = mid;
+            }
+        }
+        else
+        {
+            r--;
+        }
+    }
+
+    return nums[l] == target;
+}
